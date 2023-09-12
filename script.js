b7601b2c3577aa0d0c8ddc18894886e521c1c82a
Jordan Perkins <jordanperkins@hotmail.co.uk>
2023-09-12 20:30:06 +0100

    Finished Project

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ for (let i = 0; i < 16*16; i++){
     container.appendChild(div)
 }
 
+
+function resizeGrid(){
+    let size = prompt('Enter a grid size between 1 and 100')
+    if (size > 100 || size < 1){
+        alert('Please enter a number between 1 and 100')
+        return
+    }   
+    container.innerHTML = ''
+    for (let i = 0; i < size*size; i++){
+        const div = document.createElement('div')
+        div.classList.add('grid-item')
+        div.style.width = `${600/size}px`;
+        div.style.height = `${600/size}px`;
+        div.addEventListener('mouseover', () => { 
+            div.style.backgroundColor = 'black'
+        });
+    
+        container.appendChild(div)
+    }
+
+}
