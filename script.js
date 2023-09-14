5be768630a18eb46f893169898640c9e04eb0367
Jordan Perkins <jordanperkins@hotmail.co.uk>
2023-09-14 18:50:12 +0100

    Added Rainbow Mode

--- script.js
+++ script.js
@@ -46,4 +46,23 @@ function clearGrid(){
     })
 }
 
+
+function getRandomColour(){
+    let letters = '0123456789ABCDEF'
+    let colour = '#'
+    for (let i = 0; i < 6; i++){
+        colour += letters[Math.floor(Math.random() * 16)]
+    }
+    return colour
+}
+
+function rainbowMode(){
+    const gridItems = document.querySelectorAll('.grid-item')
+    gridItems.forEach(item => {
+        item.addEventListener('mouseover', () => { 
+            item.style.backgroundColor = getRandomColour()
+        });
+    })
+}
+
 initialGrid()
